a25afc82d08623418578aa23e15acb3da671c25c
AlexisVatin <49270565+AlexisVatin@users.noreply.github.com>
2019-12-03 11:38:36 +0100

    Update Menu.py

--- Menu.py
+++ Menu.py
@@ -33,6 +33,28 @@ def bad () :
     menu()
 
 #main routin
-
 menu()
+
+
+
+# try catch
+
+try:
+    selection == 1
+    good() # le bot est lancé
+    if str(input("Appuyez sur une touche pour arreter le bot"))
+        break
+
+try:
+    selection == 2
+    print("bad")
+try:
+    selection == 3
+    print("A bientot")
+except ValueError:
+    print("Oops!  That was no valid number.  Try again...")
+    menu()
+         # if Test1 == true:
+         #    selection2=str(input("Enter anything to return to menu"))
+         #    menu()
 
