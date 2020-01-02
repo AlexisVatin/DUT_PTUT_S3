82228f7bb6f2c201c2271ed65a5b577c3aa4cb79
AlexisVatin <49270565+AlexisVatin@users.noreply.github.com>
2020-01-02 23:15:23 +0100

    Execute le code
    Orientée objet.

---
+++ App.py
@@ -0,0 +1,23 @@
+import sys
+import getopt
+from Logic.bot import Bot
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "hf:", ["file="])
+    except getopt.GetoptError:
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == '-h':
+            print('test.py -f <file>')
+            sys.exit()
+        elif opt in ("-f", "--file"):
+            print(arg)
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+    listTempo = ["Astronogeek"] # Liste dynamique a changer apres
+    bot = Bot(listTempo, "drivers\chromedriver.exe")
+    bot.execute()
