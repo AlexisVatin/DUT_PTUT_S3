2a1aeb2ae862900c438caee7ac970792027ad378
AlexisVatin <49270565+AlexisVatin@users.noreply.github.com>
2020-01-10 22:36:58 +0100

    Update App.py

--- App.py
+++ App.py
@@ -1,24 +1,27 @@
 import sys
 import getopt
-from Logic.bot import Bot
+from gui.main_window import MainWindow
 
-def main(argv):
-    try:
-        opts, args = getopt.getopt(argv, "hf:", ["file="])
-    except getopt.GetoptError:
-        sys.exit(2)
-
-    for opt, arg in opts:
-        if opt == '-h':
-            print('test.py -f <file>')
-            sys.exit()
-        elif opt in ("-f", "--file"):
-            print(arg)
-
+# def main(argv):
+#     try:
+#         opts, args = getopt.getopt(argv, "hf:", ["file="])
+#     except getopt.GetoptError:
+#         sys.exit(2)
+#
+#     for opt, arg in opts:
+#         if opt == '-h':
+#             print('test.py -f <file>')
+#             sys.exit()
+#         elif opt in ("-f", "--file"):
+#             print(arg)
+#
+#
+# if __name__ == "__main__":
+#     main(sys.argv[1:])
+#     listTempo = ["astronogeek"] # Liste dynamique a changer apres
+#     bot = Bot(listTempo, "drivers\chromedriver.exe")
+#     bot.execute()
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
-    listTempo = ["astronogeek"] # Liste dynamique a changer apres
-    bot = Bot(listTempo, "drivers\chromedriver.exe")
-    bot.execute()
-
+    main_window = MainWindow()
+    main_window.mainloop()
